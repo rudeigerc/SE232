92678a8152003822d77f9dd34c17b7c1e6e2ea2f
rudeigerc <rudeigerc@gmail.com>
2018-11-27 22:53:29 +0800

    Update LRU Cache

--- hw/hw11/lru.h
+++ hw/hw11/lru.h
@@ -1,18 +1,79 @@
+#include <unordered_map>
+
+class RNode
+{
+  public:
+    RNode() {}
+    RNode(int k, int v) : key(k), value(v) {}
+
+    int key;
+    int value;
+    RNode *prev;
+    RNode *next;
+};
+
 class LRUCache
 {
   public:
-    LRUCache(int capacity)
+    LRUCache(int cap) : capacity(cap)
     {
-        // TODO: your code here
+        head->next = tail;
+        tail->prev = head;
     }
 
     int get(int key)
     {
-        // TODO: your code here
+        if (auto it = nodes.find(key); it != nodes.end())
+        {
+            RNode *node = nodes[key];
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+
+            node->prev = head;
+            node->next = head->next;
+            head->next = node;
+            node->next->prev = node;
+
+            return node->value;
+        }
+        return -1;
     }
 
     void put(int key, int value)
     {
-        // TODO: your code here
+        RNode *node;
+
+        if (auto it = nodes.find(key); it != nodes.end())
+        {
+            node = nodes[key];
+            node->value = value;
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+        }
+        else
+        {
+            if (nodes.size() == capacity)
+            {
+                node = tail->prev;
+                nodes.erase(tail->prev->key);
+                tail->prev = tail->prev->prev;
+                tail->prev->next = tail;
+                node->next = NULL;
+                node->prev = NULL;
+            }
+            node = new RNode(key, value);
+            nodes[key] = node;
+        }
+
+        node->prev = head;
+        node->next = head->next;
+        node->prev->next = node;
+        node->next->prev = node;
     }
+
+  private:
+    int capacity;
+    std::unordered_map<int, RNode *> nodes;
+    RNode *head = new RNode();
+    RNode *tail = new RNode();
 };
